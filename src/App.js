c2ade0bf3499afb9739aab0e8eeedee2c291d197
avinash <gudaliyaavinash123@gmail.com>
2023-12-21 09:23:40 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import axios from "axios"
+import { useEffect } from "react"
+import { useState } from "react"
+import './App.css';
+function App() {
+
+  const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get('https://rickandmortyapi.com/api/character')
+            .then((res) => {
+                console.log(res.data.results);
+                setData(res.data.results)
+            })
+            .catch((error) => {
+                alert(error);
+            })
+    }, [])
+
+  return (
+    <div className="App">
+        <table className='table'>
+                <div className="flex">
+                    {
+                        data.map((el) => {
+                            return <tr className='tr'>
+                                <td className='img'><img src={el.image} alt="" width="100%" height="220px" /></td>
+                                <td className='td'>
+                                    <ul>
+                                        <li className='Name'>{el.name}</li>
+                                        <div className="flex">
+                                            <div className="dot"style={{background: el.status == "Alive" ? "rgb(20, 226, 20)" : el.status == "Dead" ? "red" : "rgb(158, 158, 158)",}}></div>
+                                            <li className='Alive'>{el.status}-{el.species}</li>
+                                        </div>
+                                        <li className='Location'>Last known location:</li>
+                                        <li className='Expanse'>{el.location.name}</li>
+                                        <li className='Seen'>First seen in:</li>
+                                        <li className='Schwifty'>{el.origin.name}</li>
+                                    </ul>
+                                </td>
+                            </tr>
+
+                        })
+                    }
+                </div>
+            </table>
+    </div>
+  );
+}
+
+export default App;
